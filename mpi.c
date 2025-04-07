985d7f40a7cc3addab703e5d7f0d2fd4cf5eaa7d
Biz02m <mikolajbisewski@wp.pl>
2025-04-07 21:30:28 +0200

    first commit to lab_3

--- mpi.c
+++ mpi.c
@@ -3,18 +3,21 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <mpi.h>
+#include "numgen.c"
 
 int main(int argc,char **argv) {
+
   Args ins__args;
   parseArgs(&ins__args, &argc, argv);
 
-  int INITIAL_NUMBER = ins__args.start; 
-  int FINAL_NUMBER = ins__args.stop;
+  //program input argument
+  long inputArgument = ins__args.arg; 
 
   struct timeval ins__tstart, ins__tstop;
 
   int myrank,nproc;
-  
+  unsigned long int *numbers;
+
   MPI_Init(&argc,&argv);
 
   // obtain my rank
@@ -22,68 +25,13 @@ int main(int argc,char **argv) {
   // and the number of processes
   MPI_Comm_size(MPI_COMM_WORLD,&nproc);
 
-  if(!myrank) 
-      gettimeofday(&ins__tstart, NULL);
+  if(!myrank){
+      	gettimeofday(&ins__tstart, NULL);
+	numbers = (unsigned long int*)malloc(inputArgument * sizeof(unsigned long int));
+  	numgen(inputArgument, numbers);
+  }
 
   // run your computations here (including MPI communication)
-
-  if(!myrank){
-    printf("%d, %d\n", INITIAL_NUMBER, FINAL_NUMBER);
-  }
-
-
-  if(INITIAL_NUMBER == 0){
-    double pi, pi_final;
-    int mine, sign;
-    int i;
-      // each process performs computations on its part
-    pi = 0;
-    mine = myrank * 2 + 1;
-    sign = (((mine - 1) / 2) % 2) ? -1 : 1;
-
-    for (; mine < FINAL_NUMBER;)
-    {
-      //        printf("\nProcess %d %d %d", myrank,sign,mine);
-      //        fflush(stdout);
-      pi += sign / (double) mine;
-      mine += 2 * nproc;
-      sign = (((mine - 1) / 2) % 2) ? -1 : 1;
-    }
-
-      // now merge the numbers to rank 0
-    MPI_Reduce (&pi, &pi_final, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    if (!myrank)
-    {
-      pi_final *= 4;
-      printf ("pi=%.12f", pi_final);
-    }
-  } else {
-    double pi = 0, pi_final;
-    int sign = 1;
-    int range = FINAL_NUMBER/nproc;
-    int start = range * myrank * 2 + 1;
-    int mine = start;
-    
-    
-    for (int i = 0 ; i < range; i++){
-      if(!myrank){
-        //printf("%d\n", mine* sign); 
-      }
-      pi += sign/(double) mine;
-      mine += 2;
-      sign *= -1;
-    }
-
-      // now merge the numbers to rank 0
-    MPI_Reduce (&pi, &pi_final, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    if (!myrank)
-    {
-      pi_final *= 4;
-      printf ("pi=%.12f", pi_final);
-    }
-  }
 
 
   // synchronize/finalize your computations
